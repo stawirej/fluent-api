47fbc1a91272fd7c9ab73100f0293ba3ca7ebcfb
Piotr Stawirej <piotr.stawirej@gmail.com>
2020-04-13 22:58:46 +0200

    Add example.

--- src/test/java/com/github/stawirej/fluentapi/example/scenarios/fluentapi/currying/FunctionCurryingScenarios.java
+++ src/test/java/com/github/stawirej/fluentapi/example/scenarios/fluentapi/currying/FunctionCurryingScenarios.java
@@ -1,8 +1,30 @@
 package com.github.stawirej.fluentapi.example.scenarios.fluentapi.currying;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
 final class FunctionCurryingScenarios {
 
-    private FunctionCurryingScenarios() {
+    @Test
+    void multiparameterFunction() {
+        // Given
+        BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
+        // When
+        Integer sum = add.apply(1, 2);
 
+        // Then
+        then(sum).isEqualTo(3);
+    }
+
+    @Test
+    void currying() {
+        // Given
+
+        // When
+
+        // Then
     }
 }
