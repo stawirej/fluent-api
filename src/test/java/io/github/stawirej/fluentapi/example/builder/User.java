450fb0c9fda24cec54bbc605f87c917656327a41
Piotr Stawirej <piotr.stawirej@gmail.com>
2022-04-06 08:30:38 +0200

    docs: creating builder

---
+++ src/test/java/io/github/stawirej/fluentapi/example/builder/User.java
@@ -0,0 +1,97 @@
+package io.github.stawirej.fluentapi.example.builder;
+
+import static java.util.Objects.requireNonNull;
+
+public final class User {
+
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+    private String email;
+
+    private User(String name,
+                 String surname,
+                 String login,
+                 String password,
+                 String email) {
+
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public static UserBuilder user() {
+        return new UserBuilder();
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String surname() {
+        return surname;
+    }
+
+    public String login() {
+        return login;
+    }
+
+    public String password() {
+        return password;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public static class UserBuilder {
+
+        private String name;
+        private String surname;
+        private String login;
+        private String password;
+        private String email;
+
+        private UserBuilder() {
+        }
+
+        public UserBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder withSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public UserBuilder withLogin(String login) {
+            this.login = login;
+            return this;
+        }
+
+        public UserBuilder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+
+            requireNonNull(name, "name cannot be null");
+            requireNonNull(surname, "surname cannot be null");
+            requireNonNull(login, "login cannot be null");
+            requireNonNull(password, "password cannot be null");
+            requireNonNull(email, "email cannot be null");
+
+            return new User(name, surname, login, password, email);
+        }
+    }
+}
